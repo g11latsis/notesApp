6580b8c45f060fddd387f50b6b946a561b17652f
g11latsis <g11latsis@gmail.com>
2024-06-18 21:37:04 +0300

    Notes By Username

--- src/main/java/gr/aueb/NoteApp/service/exceptions/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package gr.aueb.NoteApp.service.exceptions;
-
-public class EntityNotFoundException extends Exception {
-
-    public EntityNotFoundException(Class<?> entityClass, Long id) {
-        super(entityClass.getSimpleName() + " with id " + id + " not found");
-    }
-}
